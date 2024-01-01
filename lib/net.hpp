2e7a220416e8a6d6250f6945bbf4a59394e1b369
junyoung-sim <jysim.ny@gmail.com>
2024-01-01 15:16:38 -0500

    2024-01-01

--- lib/net.hpp
+++ lib/net.hpp
@@ -65,10 +65,43 @@ public:
     void init(std::default_random_engine &seed, bool sm);
 
     std::vector<double> forward(std::vector<double> &x);
-    void backward(std::vector<double> &x, std::vector<double> &y, double alpha, double lambda);
 };
 
 double relu(double x);
 double drelu(double x);
+
+/*
+void Net::backward(std::vector<double> &x, std::vector<double> &y, double alpha, double lambda) {
+    unsigned int targ;
+    if(softmax) targ = std::max_element(y.begin(), y.end()) - y.begin();
+
+    std::vector<double> yhat = forward(x);
+
+    for(int l = layers.size() - 1; l >= 0; l--) {
+        double part = 0.00, grad = 0.00;
+        for(unsigned int n = 0; n < layers[l].out_features(); n++) {
+            if(l == layers.size() - 1 && softmax) part = yhat[n] - (n == targ);
+            else if(l == layers.size() - 1 && !softmax) part = -2.00 * (y[n] - yhat[n]);
+            else part = layers[l].node(n)->err() * drelu(layers[l].node(n)->sum());
+            
+            double updated_bias = layers[l].node(n)->bias() - alpha * part;
+            layers[l].node(n)->set_bias(updated_bias);
+
+            for(unsigned int i = 0; i < layers[l].in_features(); i++) {
+                if(l == 0) grad = part * x[i];
+                else {
+                    grad = part * layers[l-1].node(i)->act();
+                    layers[l-1].node(i)->add_err(part * layers[l].node(n)->weight(i));
+                }
+
+                grad += lambda * layers[l].node(n)->weight(i);
+
+                double updated_weight = layers[l].node(n)->weight(i) - alpha * grad;
+                layers[l].node(n)->set_weight(i, updated_weight);
+            }
+        }
+    }
+}
+*/
 
 #endif
