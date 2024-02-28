69b4037254766a48faea0b6ae2e74efa4301bf18
junyoung-sim <jysim.ny@gmail.com>
2024-02-27 23:01:18 -0500

    2024-02-27

--- python/test.py
+++ python/test.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 
+import math as math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def annualized_return(value):
+    n = 0
+    expectation = 1.00
+    returns = []
+    for t0 in range(0, value.shape[0], 251):
+        tf = min(t0 + 251, value.shape[0] - 1)
+        expectation *= 1.00 + (value[tf] - value[t0]) / value[t0]
+        returns.append(expectation)
+        n += 1
+    annualized = math.pow(expectation, 1 / n) - 1.00
+    return annualized, np.array(returns)
+
+def maximum_drawdown(value):
+    mdd = 0.00
+    dp = np.copy(value)
+    for t in range(1, value.shape[0]):
+        dp[t] = max(dp[t-1], dp[t])
+        mdd = min((value[t] - dp[t]) / dp[t], mdd)
+    return abs(mdd)
 
 if __name__ == "__main__":
     state = pd.read_csv("./res/state")
@@ -39,3 +60,13 @@ if __name__ == "__main__":
     plt.ylim(0.00, 0.70)
 
     plt.savefig("./res/test_portfolio.png")
+
+    apr, returns = annualized_return(portfolio["value"])
+    stdev = np.std(returns)
+    sharpe = apr / stdev
+    mdd = maximum_drawdown(portfolio["value"])
+
+    print(f"APR={apr}")
+    print(f"STD={stdev}")
+    print(f"SHR={sharpe}")
+    print(f"MDD={mdd}")
